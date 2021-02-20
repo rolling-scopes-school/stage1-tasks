33424aea3ca73eaa0ee9a4763b27b9a21b74955f
irinainina <irbris@mail.ru>
2021-02-20 19:35:45 +0200

    feat: add weather app

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - https://rolling-scopes-school.github.io/stage1-tasks/virtual-piano/
 - https://rolling-scopes-school.github.io/stage1-tasks/photo-filter/
 - https://rolling-scopes-school.github.io/stage1-tasks/magic-site/
+- https://rolling-scopes-school.github.io/stage1-tasks/weather/
---
+++ weather/script.js
@@ -0,0 +1,25 @@
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+  
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+}
+
+function setCity(event) {
+  if (event.code === 'Enter') {
+    getWeather();
+    city.blur();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
