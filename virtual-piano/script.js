8aa6e327fa0f70272b51f3747033bce867a50e0e
Raiuk <40764668+Raiuk@users.noreply.github.com>
2021-03-25 08:50:22 +0300

    feat: add some functionality

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -5,12 +5,45 @@ function playAudio(src) {
     audio.play()
 }
 const piano = document.querySelectorAll('.piano-key')
+const notes = ["c", "d", 'e', 'f', 'g', 'a', 'b', 'c♯', 'd♯', '', 'f♯', 'g♯', 'a♯']
+const keys = ['d', 'f', 'g', 'h', 'j', 'k', 'l', 'r', 't', 'y', 'u', 'i', 'o']
 
 
+
+console.log(piano)
 piano.forEach(el => {
-    el.addEventListener('click', () => playAudio(`assets/audio/${el.dataset.note}.mp3`))
+    el.addEventListener(('mousedown'), () => {
+        playAudio(`assets/audio/${el.dataset.note}.mp3`)
+        console.log(el)
+        el.classList.add('piano-key-active', 'piano-key-active-pseudo');
+    })
 })
 
+piano.forEach(el => {
+    el.addEventListener(('mouseup'), () => {
+        el.classList.remove('piano-key-active', 'piano-key-active-pseudo');
+    })
+    
+    el.addEventListener(('mouseout'), () => {
+        el.classList.remove('piano-key-active', 'piano-key-active-pseudo');
+    })
+})
+
+
+window.addEventListener("keydown", (el) => {
+    let keyIndex = keys.indexOf(el.key)
+    if (keyIndex != -1 && notes[keyIndex] != '') {
+        playAudio(`assets/audio/${notes[keyIndex]}.mp3`)
+        piano.item(keyIndex).classList.add('piano-key-active', 'piano-key-active-pseudo');
+    }
+})
+
+window.addEventListener("keyup", (el) => {
+    let keyIndex = keys.indexOf(el.key)
+    if (keyIndex != -1 && notes[keyIndex] != '') {
+        piano.item(keyIndex).classList.remove('piano-key-active', 'piano-key-active-pseudo');
+    }
+})
 
 function turnLabels() {
     if (!event.target.classList.contains('btn-active')) {
@@ -24,17 +57,10 @@ document.querySelectorAll(".btn-container").forEach(button => button.addEventLis
 
 
 function fullScreenMod() {
-    let element = document.documentElement;
-    element.requestFullscreen =
-        element.requestFullscreen ||
-        element.mozRequestFullscreen ||
-        element.msRequestFullscreen ||
-        element.webkitRequestFullscreen;
-
     if (!document.fullscreenElement) {
-        element.requestFullscreen()
-
-    } else {
+        document.documentElement.requestFullscreen() 
+    } 
+    else {
         if (document.exitFullscreen) {
             document.exitFullscreen();
         }
@@ -42,5 +68,4 @@ function fullScreenMod() {
 }
 
 document.querySelector('.fullscreen').addEventListener("click", () => fullScreenMod())
-
 
