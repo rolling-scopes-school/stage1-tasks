f2987d39c1a0646ead005040b7721e5d2562ad72
Piotr-reader <piotr_partsianko@inbox.ru>
2021-03-28 11:21:19 +0300

    feat: implement reset button function

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,31 +1,41 @@
 "use strict";
 
-let filters = document.querySelectorAll(".filters");
-let editor = document.querySelector(".editor");
+const fullScreen = document.querySelector(".fullscreen");
+const root = document.querySelector(":root");
+const filters = document.querySelectorAll(".filters");
+const input = document.querySelectorAll("input");
+const output = document.querySelectorAll("output");
+const btnContainer = document.querySelector(".btn-container");
 
 //  filters
 function inputValue(event) {
-  let output = document.querySelectorAll("output");
-
-  let img = document.querySelector("img");
-  img.style.setProperty(`--${event.target.name}`, event.target.value + event.target.dataset.sizing);
-
-  for (var i = 0; i < output.length; i++) {
-    if (output[i].name === event.target.name) {
-      output[i].value = event.target.value;
-    }
-  }
+  root.style.setProperty(`--${event.target.name}`, event.target.value + event.target.dataset.sizing);
+  output.forEach((el) => (el.name === event.target.name ? (el.value = event.target.value) : null));
 }
-filters.forEach((input) => input.addEventListener("input", inputValue));
+filters.forEach((event) => event.addEventListener("input", inputValue));
 
 // fullScreen
-const fullScreen = document.querySelector(".fullscreen");
-const elem = document.documentElement;
-
 fullScreen.addEventListener("mousedown", () => {
+  const elem = document.documentElement;
   if (document.fullscreenElement === null) {
     elem.requestFullscreen();
   } else {
     document.exitFullscreen();
+  }
+});
+
+// reset btn
+function btnReset() {
+  input.forEach((input) => {
+    input.value = input.defaultValue;
+    root.style.setProperty(`--${input.name}`, input.value + input.dataset.sizing);
+  });
+  output.forEach((el) => (el.value = el.defaultValue));
+}
+
+// all btn
+btnContainer.addEventListener("mousedown", (event) => {
+  if (event.target.classList.contains("btn-reset")) {
+    btnReset();
   }
 });
