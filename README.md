1a7bf6ede6291549e7860b6628f3851c8f0f975a
irinainina <irbris@mail.ru>
2021-05-28 20:00:33 +0300

    feat: add momentum demo

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 - https://rolling-scopes-school.github.io/stage1-tasks/virtual-piano/
 - https://rolling-scopes-school.github.io/stage1-tasks/photo-filter/
 - https://rolling-scopes-school.github.io/stage1-tasks/sounds-app/
+- https://rolling-scopes-school.github.io/stage1-tasks/momentum/
 - https://rolling-scopes-school.github.io/stage1-tasks/weather/
---
+++ momentum/js/playList.js
@@ -0,0 +1,22 @@
+const playList = [
+  {      
+    title: 'Aqua Caelestis',
+    src: '../assets/sounds/Aqua Caelestis.mp3',
+    duration: '00:58'
+  },  {      
+    title: 'River Flows In You',
+    src: '../assets/sounds/River Flows In You.mp3',
+    duration: '03:50'
+  },
+  {      
+    title: 'Summer Wind',
+    src: '../assets/sounds/Summer Wind.mp3',
+    duration: '05:05'
+  },
+  {      
+    title: 'Ennio Morricone',
+    src: '../assets/sounds/Ennio Morricone.mp3',
+    duration: '05:03'
+  }
+]
+export default playList;
