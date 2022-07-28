2d86ccae7bdd620cf4707ebe4b46f6b1dc35eb4c
Alexey Kuptsov <60064512+jerubrin@users.noreply.github.com>
2022-07-28 09:28:49 +0600

    feat: add playlist

--- src/js/AudioPlayer.js
+++ src/js/AudioPlayer.js
@@ -12,23 +12,15 @@ export default class AudioPlayer {
         this.playNextBtn = document.querySelector('button.play-next')
 
         this.playBtn.onclick = () => {
-            this.playBtn.classList.toggle('pause');
-            if(this.isPlay) {
-                this.pauseAudio()
-                this.isPlay = false;
+            if(this.isPlay) { 
+                this.pauseAudio() 
             } else {
                 this.playAudio(this.currentFileNum);
-                this.isPlay = true;
-            }   
+            }
         }
 
         this.playNextBtn.onclick = () => { 
-            if (this.currentFileNum < files.length - 1) { 
-                this.currentFileNum++ 
-            } else { 
-                this.currentFileNum = 0
-            }
-            if (this.isPlay) { this.playAudio(this.currentFileNum) }
+            this.playNext()
         }
         this.playPrevBtn.onclick = () => { 
             if (this.currentFileNum > 0) { 
@@ -37,16 +29,48 @@ export default class AudioPlayer {
                 this.currentFileNum = files.length - 1
             }
             if (this.isPlay) { this.playAudio(this.currentFileNum) }
-         }
+        }
+        //TODO: 
+        this.audio.onended = () => { this.playNext() }
+
+        //playlist
+        this.playList = document.querySelector(".play-list")
+        let liList = []
+        for(let file of files) {
+            let li = document.createElement('li');
+            li.classList.add('play-item')
+            li.textContent = file.name
+            let num = files.indexOf(file)
+            li.onclick = () => {
+                this.playAudio(num)
+            }
+            this.playList.append(li)
+        }
+    }
+
+    playNext() {
+        if (this.currentFileNum < files.length - 1) { 
+            this.currentFileNum++
+        } else { 
+            this.currentFileNum = 0
+        }
+        if (this.isPlay) { this.playAudio(this.currentFileNum) }
     }
 
     playAudio(n) {
         n = n ? n : 0
         this.audio.src = `assets/sounds/src/assets/sounds/${files[n].filename}`
-        this.audio.currentTime = 0;
-        this.audio.play();
+        this.audio.currentTime = 0
+        this.audio.play()
+        this.isPlay = true
+        this.playBtn.classList.add('pause');
+        for(let file of this.playList.childNodes) { file.classList.remove('item-active') }
+        this.playList.childNodes[n].classList.add('item-active')
+        this.currentFileNum = n
     }
     pauseAudio() {
-        this.audio.pause();
+        this.audio.pause()
+        this.isPlay = false
+        this.playBtn.classList.remove('pause');
     }
 }
